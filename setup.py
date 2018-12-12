8d9b36da7fffdb719ffc3deaf93ad16eb20a5607
irotem <rotemb@gmail.com>
2018-12-12 18:36:06 +0200

    Pyelftools instead of elftools

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='inspectelf',
       author_email='',
       license='',
       packages=['inspectelf'],
-      install_requires=['capstone', 'elftools'],
+      install_requires=['capstone', 'pyelftools'],
       zip_safe=False)
