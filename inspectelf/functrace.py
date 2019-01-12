5929acccf9f2922646c4856cc139151a89464e09
Gili Yankovitch <giliy@nyxsecurity.net>
2019-01-13 01:35:42 +0200

    WIP Working (?) function detection

--- inspectelf/functrace.py
+++ inspectelf/functrace.py
@@ -36,7 +36,7 @@ def import_funcs(elffile):
 		raise Exception("No string table in ELF file for relocatables")
 
 	# Relocs index start from 1 somehow :S
-	idx = 1
+	idx = 2
 	startoff = 0
 
 	# Per architecture mods
@@ -132,7 +132,7 @@ def find_usages(elffile, imports):
 				if hex(offset) in i.op_str:
 					print "0x%x: Call to %s@plt" % (i.address, offset_map[offset])
 
-def find_functions_aarch64(section, found_funcs):
+def find_functions_aarch64(section, found_funcs, terminating = []):
 	md = Cs(CS_ARCH_ARM64, CS_MODE_ARM)
 
 	md.skipdata = True
@@ -156,17 +156,37 @@ def find_functions_aarch64(section, found_funcs):
 	 	]
 
 	calls = [
-			{"mnemonic": "bl", "arg": "#(0x[0-9a-f])"}
+			{"mnemonic": "bl", "arg": "#(0x[0-9a-f]+)"}
+		]
+	nops = [
+			bytes("\x00\x00\x00\x00"),
+			bytes("\x1f\x20\x03\xd5")
 		]
 
-	print "Section address: 0x%x" % section.header.sh_addr
+	# print "Section address: 0x%x" % section.header.sh_addr
 
 
 	for i in md.disasm(section.data(), section.header.sh_addr):
+		# print hex(i.address), "(0x%x-0x%x)" % (addresses[0],addresses[-1]), i.mnemonic, i.op_str
+
+		# Skip NOPs in function prolog
+		if len(addresses) == 1 and addresses[0] == i.address:
+			skip_nop = False
+			# print str(i.bytes).encode("hex")
+			for nop in nops:
+				if i.bytes == nop:
+					#@if len(addresses) == 1 and addresses[0] == i.address and i.mnemonic == "nop":
+					addresses = [i.address + 4]
+					skip_nip = True
+					break
+
+			if skip_nop:
+				continue
+
 		# Is this the end of the function?
 		# Marked as a return clause that has no referenced addresses further ahead
 		# in the assembly
-		if i.mnemonic == "ret" and i.address > addresses[-1]:
+		if i.mnemonic == "ret" and i.address >= addresses[-1]:
 			if addresses[0] in expected_functions:
 				print "Found expected function: 0x%x" % addresses[0]
 
@@ -175,7 +195,7 @@ def find_functions_aarch64(section, found_funcs):
 			# Found a function end.
 			functions[addresses[0]] = i.address - addresses[0]
 
-			print "Found function (RET): 0x%x (%d)" % (i.address, i.address - addresses[0])
+			print "Found function (RET): 0x%x (%d)" % (addresses[0], i.address - addresses[0])
 
 			# Start looking at a new function
 			addresses = [i.address + 4]
@@ -192,13 +212,27 @@ def find_functions_aarch64(section, found_funcs):
 				if m is None:
 					continue
 
-				# print i.address, "\t", i.mnemonic, i.op_str
-				expected_functions.append(m.groups()[0])
+				nextaddr = int(m.groups()[0], 16)
+
+				if nextaddr in terminating:
+					if addresses[0] in expected_functions:
+						print "Found expected function: 0x%x" % addresses[0]
+
+						expected_functions.remove(addresses[0])
+
+					# Found a function end.
+					functions[addresses[0]] = i.address - addresses[0]
+
+					print "Found function (TERMINATING CALL): 0x%x (%d)" % (addresses[0], i.address - addresses[0])
+
+					# Start looking at a new function
+					addresses = [i.address + 4]
+				else:
+					if nextaddr not in expected_functions:
+						expected_functions.append(nextaddr)
 
 		for b in branches:
 			if i.mnemonic == b["mnemonic"]:
-				if i.address == 0x425bdc:
-					print "Branching!"
 				# Look for the argument
 				m = re.match(b["arg"], i.op_str)
 
@@ -207,7 +241,7 @@ def find_functions_aarch64(section, found_funcs):
 
 				nextaddr = int(m.groups()[0], 16)
 
-				# This is a DECLARED FUNCTION that I've found
+				# This is a DECLARED FUNCTION that I've found. Ignore this branch.
 				if nextaddr in found_funcs:
 					break
 
@@ -216,7 +250,7 @@ def find_functions_aarch64(section, found_funcs):
 					break
 
 				# Check if it's the furthest branch and is pointing back inside the function
-				if (i.address > addresses[-1]) and (i.address > nextaddr) and (i.address > addresses[-1]):
+				if ((i.address >= addresses[-1]) and (i.address >= nextaddr) and (i.address >= addresses[-1])) or (nextaddr in terminating):
 					if addresses[0] in expected_functions:
 						print "Found expected function: 0x%x" % addresses[0]
 
@@ -226,7 +260,7 @@ def find_functions_aarch64(section, found_funcs):
 					# if i.address > 0x423230:
 					functions[addresses[0]] = i.address - addresses[0]
 
-					print "Found function (BACK JMP): 0x%x (%d)" % (i.address, i.address - addresses[0])
+					print "Found function (BACK JMP): 0x%x (%d)" % (addresses[0], i.address - addresses[0])
 
 					# Start looking at a new function
 					addresses = [i.address + 4]
@@ -236,8 +270,19 @@ def find_functions_aarch64(section, found_funcs):
 					addresses.sort()
 				break
 
-	print "Functions:", functions
-	print "Expected (unfound) functions:", expected_functions
+	k = functions.keys()
+	k.sort()
+	for x in functions:
+		if x in expected_functions:
+			expected_functions.remove(x)
+
+	for x in found_funcs:
+		if x in expected_functions:
+			expected_functions.remove(x)
+
+	expected_functions.sort()
+	print "Functions:", [ (hex(x), functions[x]) for x in k ]
+	print "Expected (unfound) functions:", [ hex(x) for x in expected_functions ]
 
 def find_functions(elffile):
 	elf = ELFFile(open(elffile, "rb"))
@@ -258,8 +303,23 @@ def find_functions(elffile):
 		if sym.entry.st_value != 0:
 			found_funcs.append(sym.entry.st_value)
 
+	imports = import_funcs(elffile)
+
+	# Add imported functions
+	found_funcs += [ x["offset"] for x in imports ]
+
+	# filter terminating branches
+	terminating = ["abort"]
+	terminating_funcs = filter(lambda x: x is not None, [ x["offset"] if x["name"] in terminating else None for x in imports ])
+
+	for func in imports:
+		if func["name"] in terminating:
+			print "%s@plt = 0x%x" % (func["name"], func["offset"])
+
+	print "Found Imports:", [hex(x) for x in found_funcs]
+
 	if elf.header.e_machine == "EM_AARCH64":
-		return find_functions_aarch64(text, found_funcs)
+		return find_functions_aarch64(text, found_funcs, terminating_funcs)
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
